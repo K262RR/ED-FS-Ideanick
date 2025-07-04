aa04aa831c3d7cf4fee48e7c3f7d03eb31baaa0c
Ramazan <ramazan.kubalov@gmail.com>
2025-07-04 22:16:08 +0300

    7.2 Добавлям CSS стили с применением SCSS

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import pluginJs from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import pluginImport from 'eslint-plugin-import'
-import prettierPlugin from 'eslint-plugin-prettier'
-import eslintReact from 'eslint-plugin-react'
-import pluginReact from 'eslint-plugin-react'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginImport from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintReact from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 // import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
   },
   {
-    ignores: ['node_modules', 'dist'],
+    ignores: ["node_modules", "dist"],
   },
   {
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      "@typescript-eslint": tseslint.plugin,
       react: eslintReact,
       // 'react-hooks': eslintReactHooks,
       // 'react-refresh': eslintReactRefresh,
@@ -35,29 +35,32 @@ export default [
     rules: {
       ...prettierPlugin.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
-      'react/react-in-jsx-scope': 'off',
-      'import/order': [
-        'error',
+      "react/react-in-jsx-scope": "off",
+      "import/order": [
+        "error",
         {
           alphabetize: {
-            order: 'asc',
+            order: "asc",
             caseInsensitive: false,
-            orderImportKind: 'asc',
+            orderImportKind: "asc",
           },
         },
       ],
-      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-      '@typescript-eslint/strict-boolean-expressions': 'off',
-      '@typescript-eslint/prefer-nullish-coalescing': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/triple-slash-reference': 'off',
-      '@typescript-eslint/ban-types': 'off',
-      '@typescript-eslint/consistent-type-assertions': 'off',
-      'jsx-a11y/anchor-is-valid': 'off',
-      curly: ['error', 'all'],
-      'no-irregular-whitespace': ['error', { skipTemplates: true, skipStrings: true }],
-      'no-console': ['error', { allow: ['info', 'error', 'warn'] }],
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "jsx-a11y/anchor-is-valid": "off",
+      curly: ["error", "all"],
+      "no-irregular-whitespace": [
+        "error",
+        { skipTemplates: true, skipStrings: true },
+      ],
+      "no-console": ["error", { allow: ["info", "error", "warn"] }],
     },
   },
-]
+];
