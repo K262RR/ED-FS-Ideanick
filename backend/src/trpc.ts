66845a77f7b2f6967b4c2eef20932e2955368d30
Ramazan Kubalov <ramazan.kubalov@gmail.com>
2025-07-04 18:26:43 +0300

    6.2 Знакомство с Zod и создание tRPC процедуры с инпутом
    Тоже плохо понял

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from "@trpc/server";
 import _ from "lodash";
+import { z } from "zod";
 
 const ideas = _.times(100, (i) =>({
   nick: `cool-idea-nick-${i+1}`,
@@ -8,12 +9,20 @@ const ideas = _.times(100, (i) =>({
   text: _.times(100, (j) => `<p>Text paragraph ${j+1} of idea ${i+1}...</p>`).join(""),
 }));
 
-
 const trpc = initTRPC.create();
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ["nick", "name", "description"])) };
+  }),
+  
+  getIdea: trpc.procedure.input(
+    z.object({
+      ideaNick: z.string(),
+    }),
+  ).query(({input}) => {
+   const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+   return {idea: idea || null}
   }),
 });
 
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,16 +1,35 @@
 import { useParams } from "react-router-dom";
 import { viewIdeaRouteParams } from "../../lib/routes";
+import { trpc } from "../../lib/trpc";
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as viewIdeaRouteParams;
+
+  const { data, error, isLoading, isFetching, isError } =
+  trpc.getIdea.useQuery({ideaNick});
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  if (!data?.idea) {
+    return <span>Idea not found...</span>;
+  }
+
   return (
     <div>
-      <h1>{ ideaNick }</h1>
-      <p>Description of idea 1</p>
+      <h1>{ data.idea.name }</h1>
+      <p>{ data.idea.description }</p>
       <div>
-        <p>Text paragraph 1 of idea 1</p>
-        <p>Text paragraph 2 of idea 1</p>
-        <p>Text paragraph 3 of idea 1</p>
+        <p dangerouslySetInnerHTML={{__html: data.idea.text}} />
       </div>
     </div>
   );
