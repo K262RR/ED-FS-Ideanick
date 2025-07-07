de4ece3ab63f5dd9d93b6d4d48eed12163f40cc9
Ramazan Kubalov <ramazan.kubalov@gmail.com>
2025-07-07 13:26:27 +0300

    8.1 Форма: База

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,10 +1,70 @@
+import { useState } from "react";
+import { Segment } from "../../components/Segment";
 import css from "./index.module.scss";
 
 export const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    name: "",
+    nick: "",
+    description: "",
+    content: "",
+  });
+
   return (
-    <div>
-      <h1 className={css.title}>New Idea</h1>
-      <div>Form will be here...</div>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.info(state);
+        }}
+      >
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            value={state.name}
+            onChange={(e) => setState({ ...state, name: e.target.value })}
+            name="name"
+            id="name"
+          />
+        </div>
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="nick">Nick</label>
+          <br />
+          <input
+            type="text"
+            value={state.nick}
+            onChange={(e) => setState({ ...state, nick: e.target.value })}
+            name="nick"
+            id="nick"
+          />
+        </div>
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="description">Description</label>
+          <br />
+          <input
+            type="text"
+            value={state.description}
+            onChange={(e) =>
+              setState({ ...state, description: e.target.value })
+            }
+            name="description"
+            id="description"
+          />
+        </div>
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="content">Content</label>
+          <br />
+          <textarea
+            value={state.content}
+            onChange={(e) => setState({ ...state, content: e.target.value })}
+            name="content"
+            id="content"
+          />
+        </div>
+        <button type="submit"> Отправить</button>
+      </form>
+    </Segment>
   );
 };
