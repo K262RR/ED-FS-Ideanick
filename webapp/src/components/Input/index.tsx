5853a8eef9c6f3f6273091251d5b5a789a7a027b
Ramazan Kubalov <ramazan.kubalov@gmail.com>
2025-07-07 16:02:23 +0300

    8.5 Форма: забота о UX
    Добавлено управление состоянием touched для компонентов Input и Textarea, улучшена валидация ошибок при потере фокуса.

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -11,18 +11,20 @@ export const Input = ({
 }) => {
   const value = formik.values[name];
   const error = formik.errors[name] as string | undefined;
+  const touched = formik.touched[name];
   return (
     <div style={{ marginBottom: 10 }}>
-    <label htmlFor={value}>{label}</label>
-    <br />
-    <input
-      type="text"
-      value={value.text}
-      onChange={(e) => formik.setFieldValue(name, e.target.value)}
-      name={name}
-      id={name}
-    />
-    {error && <div style={{ color: "red" }}>{error}</div>}
-  </div>
+      <label htmlFor={value}>{label}</label>
+      <br />
+      <input
+        type="text"
+        value={value.text}
+        onChange={(e) => formik.setFieldValue(name, e.target.value)}
+        onBlur={() => formik.setFieldTouched(name, true)}
+        name={name}
+        id={name}
+      />
+      {touched && error && <div style={{ color: "red" }}>{error}</div>}
+    </div>
   );
 };
--- webapp/src/components/Textarea/index.tsx
+++ webapp/src/components/Textarea/index.tsx
@@ -11,17 +11,20 @@ export const Textarea = ({
 }) => {
   const value = formik.values[name];
   const error = formik.errors[name] as string | undefined;
+  const touched = formik.touched[name];
+
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
       <br />
       <textarea
         onChange={(e) => formik.setFieldValue(name, e.target.value)}
+        onBlur={() => formik.setFieldTouched(name, true)}
         value={value.text}
         name={name}
         id={name}
       />
-      {error && <div style={{ color: "red" }}>{error}</div>}
+      {touched && error && <div style={{ color: "red" }}>{error}</div>}
     </div>
   );
 };
